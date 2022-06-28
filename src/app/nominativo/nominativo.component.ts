04905f35254b6138d1b86dd96a6dac62067dcc5d
Bakko000 <corrado.baccheschi@gmail.com>
2022-06-28 19:58:02 +0200

    1 file modified, 5 added

---
+++ src/app/nominativo/nominativo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nominativo',
+  templateUrl: './nominativo.component.html',
+  styleUrls: ['./nominativo.component.css']
+})
+export class NominativoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/prenotazione/prenotazione.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prenotazione',
+  templateUrl: './prenotazione.component.html',
+  styleUrls: ['./prenotazione.component.css']
+})
+export class PrenotazioneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
