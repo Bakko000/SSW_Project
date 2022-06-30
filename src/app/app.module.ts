d30c92c689f6d772d5a2afe2864d2494ec0dd4e7
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2022-06-30 15:33:19 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DbService } from './db.service';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PrenotazioneComponent } from './prenotazione/prenotazione.component';
 import { NominativoComponent } from './nominativo/nominativo.component';
 import { NuovoTeatroComponent } from './nuovoteatro/nuovoteatro.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports:      [ BrowserModule, HttpClientModule],
   declarations: [ AppComponent, NuovoTeatroComponent, PrenotazioneComponent, NominativoComponent],
   bootstrap:    [ AppComponent ],
   providers:    [ DbService ]
