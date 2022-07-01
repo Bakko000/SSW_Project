541580dffca593d1f4736217244adef44092165c
Bakko000 <corrado.baccheschi@gmail.com>
2022-07-01 21:55:28 +0200

    1 file modified

--- src/app/prenotazione/prenotazione.component.ts
+++ src/app/prenotazione/prenotazione.component.ts
@@ -20,9 +20,9 @@ export class PrenotazioneComponent implements OnInit {
   @Input() prenotazione: string;
 
   nfile = <number>7;
+  prenotazioni: [];
   nposti = <number>10;
-  platea = Array(this.nfile).fill('').map(() => Array(this.nposti).fill('x'));
-  btn: HTMLButtonElement;
+  platea: Array<Array<string>> = new Array(this.nfile).fill('').map(() => new Array(this.nposti).fill('x'));
 
   constructor(private renderer: Renderer2) {}
 
@@ -32,12 +32,12 @@ export class PrenotazioneComponent implements OnInit {
     this.platea[3][4]= 'Gianna';
     this.prenotazioni = this.platea.map((fila, i) => {
       this.p = fila.map((nome, j) => {
-        this.btn = this.renderer.createElement('button');
-        this.renderer.appendChild(this.prenota.nativeElement, this.btn);
-        this.btn.style.color = nome !== 'x' ? 'red' : 'green';
-        this.btn.innerHTML = 'P' + (<number>j + 1) + (<number>i + 1);
-        this.btn.value = nome == 'x' ? 'Libero' : nome;
-        this.btn.addEventListener('click', this.mostraNome);
+        btn = <HTMLButtonElement>this.renderer.createElement('button');
+        this.renderer.appendChild(this.prenota.nativeElement, btn);
+        btn.style.color = nome !== 'x' ? 'red' : 'green';
+        btn.innerHTML = 'P' + (<number>j + 1) + (<number>i + 1);
+        btn.value = nome == 'x' ? 'Libero' : nome;
+        btn.addEventListener('click', this.mostraNome);
         return this.btn;
       });
     });
