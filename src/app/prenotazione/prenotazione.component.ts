a63455c4483c4da03db8494086f161ff412cf3f8
Bakko000 <corrado.baccheschi@gmail.com>
2022-07-05 11:38:30 +0200

    2 files modified

--- src/app/prenotazione/prenotazione.component.ts
+++ src/app/prenotazione/prenotazione.component.ts
@@ -10,23 +10,17 @@ export class PrenotazioneComponent implements OnInit {
   @Input() chiave: string;
   @Input() prenotazione: string;
 
-  nfile = 5;
-  nposti = 10;
-  p: any[];
-  c: any[];
-  fila: any[];
+  nfile = 6;
+  nposti = 6;
   platea=Array(this.nfile).fill("").map(() => Array(this.nposti).fill("x"));
   
   nomeEl: HTMLElement;
-
-  prenotazioni = this.platea.map( (fila) => this.platea)
 
   constructor() { }
 
   ngOnInit() {
     this.platea[2][1] = 'Alessio';
     this.platea[3][4] = 'Gianna';
-    console.log(this.prenotazioni);
     /*this.prenotazioni = this.platea.map( (fila,i) => {
      this.p=fila.map( (nome,j) => {
       })
