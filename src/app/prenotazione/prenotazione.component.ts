6211c4f6e82edaff1476efd2596b9f224711c334
Bakko000 <corrado.baccheschi@gmail.com>
2022-07-05 12:44:17 +0200

    2 files modified

--- src/app/prenotazione/prenotazione.component.ts
+++ src/app/prenotazione/prenotazione.component.ts
@@ -2,45 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Teatro } from '../app.component';
 import { DbService } from '../db.service';
 
-export class MostraTeatro {
-  prenotazione = [];
-  bookerid: string;
-  constructor(posti, settore, bookerid) {
-    this.bookerid = bookerid;
-    var element = document.getElementById(settore);
-    this.prenotazione = posti.map((fila, i) => {
-      var p = fila.map((nome, j) => {
-        var btn = document.createElement('button');
-        if (settore == 'platea') {
-          btn.setAttribute('id', 'Platea-P' + (j + 1) + (i + 1));
-        } else {
-          btn.setAttribute('id', 'Palchi-P' + (j + 1) + (i + 1));
-        }
-        element.appendChild(btn);
-        btn.style.color = nome !== 'x' ? 'red' : 'green';
-        btn.innerHTML = 'P' + (j + 1) + (i + 1);
-        btn.value = nome == 'x' ? 'Libero' : nome;
-        btn.addEventListener('click', this.selezionaPosto, false);
-        btn.booker = this.bookerid;
-        return btn;
-      });
-      element.appendChild(document.createElement('br'));
-      return p;
-    });
-  }
-
-  private selezionaPosto(event) {
-    const nomeEl = document.getElementById('nome');
-    if (this.style.color != 'red') {
-      this.style.color = 'red';
-      nomeEl.innerHTML = event.currentTarget.booker + ' ha prenotato il posto <i>' + this.id + '</i>';
-      this.value = event.currentTarget.booker;
-    } else {
-      nomeEl.innerHTML = this.value;
-    }
-  }
-}
-
 @Component({
   selector: 'app-prenotazione',
   templateUrl: './prenotazione.component.html',
@@ -50,12 +11,14 @@ export class PrenotazioneComponent implements OnInit {
   @Input() chiave: string;
   @Input() bookerid: string = '';
   repeat: boolean = false;
+  platea: any[] = [];
+  palchi: any[] = [];
 
   constructor(private db: DbService) {}
 
   public Prenotare() {
 
-    if(!this.repeat) {
+    if(!this.repeat) {  // Non si deve ripetere per più di una volta
 
     this.db.getTheatre(this.chiave).subscribe({
       next: (content: any) => {
@@ -65,9 +28,8 @@ export class PrenotazioneComponent implements OnInit {
           var npostiplateaj = json.npostiplatea;
           var nfileplateaj = json.nfileplatea;
           var MyTheatre = new Teatro([],[], npostiplateaj, nfileplateaj, npostipalchij, nfilepalchij);
-
-          var plateaPrenotazione = new MostraTeatro(MyTheatre.platea, 'platea', this.bookerid);
-          var palchiPrenotazione = new MostraTeatro(MyTheatre.palchi, 'palchi', this.bookerid);
+          this.platea= MyTheatre.platea;
+          this.palchi = MyTheatre.palchi;
       },
       error: (err) => {
         console.error(err.error);
@@ -76,6 +38,5 @@ export class PrenotazioneComponent implements OnInit {
     this.repeat = true;
   }
 }
-
   ngOnInit() {}
 }
