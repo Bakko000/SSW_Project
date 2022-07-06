9e0eef57d20a6afeabd2d4f36130576dac49e5d2
Bakko000 <corrado.baccheschi@gmail.com>
2022-07-06 17:27:37 +0200

    1 file modified

--- src/app/nuovoteatro/nuovoteatro.component.ts
+++ src/app/nuovoteatro/nuovoteatro.component.ts
@@ -14,10 +14,10 @@ export class NuovoTeatroComponent implements OnInit {
   constructor(private db: DbService) {}
 
   public newTheatre() {
-    this.clicked = true;
-    this.newtheatre = true;
+    this.clicked = true;   // Fai apparire il form
+    this.newtheatre = true;  // Comunica che si sta creando un nuovo teatro
     const output = <HTMLElement>document.getElementById('output');
-    this.db.newKey().subscribe({
+   this.db.newKey().subscribe({
       next: (content: any) => {
         this.chiave = content;
       },
@@ -29,20 +29,19 @@ export class NuovoTeatroComponent implements OnInit {
 
   postiplatea: string;
   postipalchi: string;
-  filepalchi: number = 4;
-  fileplatea: number = 7;
+  dimensioni: any[] = [];
 
   public CreateTheatre() {
-    this.clicked = false;
-    var NuovoTeatro = new Teatro([],[], parseInt(this.postiplatea), this.fileplatea,  parseInt(this.postipalchi),this.filepalchi);
-    this.db.setTheatre(this.chiave, NuovoTeatro).subscribe({
-      next: (content: any) => {
-        console.log(content);
-      },
+    this.clicked = false;  // Fai sparire il form
+    this.dimensioni[0] = this.postiplatea;
+    this.dimensioni[1] = this.postipalchi;
+    var NuovoTeatro = new Teatro([],[], parseInt(this.postiplatea), 7,  parseInt(this.postipalchi), 4);
+    var prenotazione = this.dimensioni.concat(NuovoTeatro.platea).concat(NuovoTeatro.palchi);
+   this.db.setTheatre(this.chiave, prenotazione).subscribe({
       error: (err) => {
         console.error(err.error);
       },
-    });
+    }); 
   }
   ngOnInit() {}
 }
